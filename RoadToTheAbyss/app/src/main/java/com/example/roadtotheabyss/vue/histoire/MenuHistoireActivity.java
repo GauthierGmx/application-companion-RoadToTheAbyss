afe70485a94eea14f2925c49db6798cd982f49b6
Gauthier GOUMEAUX <gauthier.uzos@orange.fr>
2023-04-15 21:15:12 +0200

    Interface reliées par les clics boutons

--- RoadToTheAbyss/app/src/main/java/com/example/roadtotheabyss/vue/histoire/MenuHistoireActivity.java
+++ RoadToTheAbyss/app/src/main/java/com/example/roadtotheabyss/vue/histoire/MenuHistoireActivity.java
@@ -1,8 +1,13 @@
 package com.example.roadtotheabyss.vue.histoire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import com.example.roadtotheabyss.R;
+import com.example.roadtotheabyss.vue.MainActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuHistoireActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MenuHistoireActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_histoire);
+        ecouteMenu((Button) findViewById(R.id.bConfirmationHistoire), EnJeuHistoireActivity.class);
+    }
+
+    private void ecouteMenu(Button btn, final Class classe) {
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent (MenuHistoireActivity.this, classe);
+                startActivity(intent);
+            }
+        });
     }
 }
--- RoadToTheAbyss/app/src/main/java/com/example/roadtotheabyss/vue/regles/MenuReglesActivity.java
+++ RoadToTheAbyss/app/src/main/java/com/example/roadtotheabyss/vue/regles/MenuReglesActivity.java
@@ -1,8 +1,13 @@
 package com.example.roadtotheabyss.vue.regles;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import com.example.roadtotheabyss.R;
+import com.example.roadtotheabyss.vue.MainActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuReglesActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MenuReglesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_regles);
+        ecouteMenu((Button) findViewById(R.id.bCartes), ReglesCartesActivity.class);
+        ecouteMenu((Button) findViewById(R.id.bVehicule), ReglesVehiculeActivity.class);
+    }
+
+    private void ecouteMenu(Button btn, final Class classe) {
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent (MenuReglesActivity.this, classe);
+                startActivity(intent);
+            }
+        });
     }
 }
